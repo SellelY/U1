d66d85f61ae5dbd2f87b0c543227944114e07b6b
Pernilla Nadie <pernilla.nadie@outlook.com>
2023-02-26 23:52:10 +0100

    dsfeswf

--- js/quiz.js
+++ js/quiz.js
@@ -29,57 +29,67 @@ function quizPage() {
     })
 
         
-        async function getRandomDogImage() {
-
-            const messageDiv = document.querySelector("#message");
-            const _status = document.querySelector(".status");
-            const closeButton = document.querySelector(".close");
-            closeButton.style.display = "none";
-
-            messageDiv.classList.remove("hidden");
-            _status.textContent = "Loading new dog-image...";
+    async function getRandomDogImage() {
+        const messageDiv = document.querySelector("#message");
+        const _status = document.querySelector(".status");
+        const closeButton = document.querySelector(".close");
+        closeButton.style.display = "none";
+      
+        messageDiv.classList.remove("hidden");
+        _status.textContent = "Loading new dog-image...";
         
-            
-
-            async function fetchRandomImage() {
-                const randomBreed = ALL_BREEDS[Math.floor(Math.random() * ALL_BREEDS.length)];
-                const url = `https://dog.ceo/api/breed/${randomBreed.url}/images/random`;
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error(`Failed to fetch image: ${response.status} ${response.statusText}`);
-                }
-                messageDiv.classList.add("hidden");
-                const data = await response.json();
-                return data.message;
-                }
-
-                const imageUrl = await fetchRandomImage();
-                const imgElement = document.getElementById("dog-image");
-                imgElement.setAttribute("src", imageUrl);
-
-                
-
+        async function fetchRandomImage() {
+          const randomBreed = ALL_BREEDS[Math.floor(Math.random() * ALL_BREEDS.length)];
+          const url = `https://dog.ceo/api/breed/${randomBreed.url}/images/random`;
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`Failed to fetch image: ${response.status} ${response.statusText}`);
+          }
+          messageDiv.classList.add("hidden");
+          const data = await response.json();
+          return {
+            imageUrl: data.message,
+            breedName: randomBreed.name
+          };
         }
-    
-        getRandomDogImage();
-
-        const breedData = ALL_BREEDS[Math.floor(Math.random() * ALL_BREEDS.length)];
+      
+        const { imageUrl, breedName } = await fetchRandomImage();
+        const imgElement = document.getElementById("dog-image");
+        imgElement.setAttribute("src", imageUrl);
+      
         const buttonContainer = document.querySelector(".button-container");
         const buttons = buttonContainer.querySelectorAll(".quiz-answers");
-
+      
         buttons.forEach(button => {
-            button.textContent = getRandomBreedName();
-
-            if(button.textContent === breedData.name) {
-                button.classList.add("correct");
-            }
+          button.textContent = getRandomBreedName();
+          if (button.textContent === breedName) {
+            button.setAttribute("data-breed-name", breedName);
+          }
         });
-
+      
         function getRandomBreedName() {
-  const randomIndex = Math.floor(Math.random() * ALL_BREEDS.length);
-  return ALL_BREEDS[randomIndex].name;
-}
-  
+          const randomIndex = Math.floor(Math.random() * ALL_BREEDS.length);
+          return ALL_BREEDS[randomIndex].name;
+        }
+
+        buttons.forEach((button, index) => {
+            button.textContent = ALL_BREEDS[index].name;
+            if (ALL_BREEDS[index].name === breedName) {
+              button.classList.add("correct");
+            }
+        
+            button.addEventListener("click", () => {
+              if (button.classList.contains("correct")) {
+                console.log("CORRECT!");
+              } else {
+                console.log("incorrect");
+              }
+            });
+          });
+        }
+        
+      
+      getRandomDogImage();
       
 }
 
